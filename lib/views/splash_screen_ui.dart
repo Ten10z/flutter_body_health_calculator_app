4ad907a1f5864942ed3b8ec62614d2825b698995
Ten10z <s6619410063@sau.ac.th>
2026-01-28 21:10:15 +0700

    Commit02: ทำแถบด้านล่างเสร็จแล้ว

---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/home_ui.dart';
+
+class SplashScreenUi extends StatefulWidget {
+  const SplashScreenUi({super.key});
+
+  @override
+  State<SplashScreenUi> createState() => _SplashScreenUiState();
+}
+
+class _SplashScreenUiState extends State<SplashScreenUi> {
+
+  @override
+  void initState() {
+    //ให้หน่วงเวลา 3 วินาทีแล้วเปิดไปหน้า HomeUi แบบย้อนกลับไม่ได้
+    Future.delayed(
+      //เวลาที่หน่วง
+      Duration(seconds: 3),       
+      //เมื่อครบเวลาแล้วจะให้ทำอะไร
+      (){
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeUi(),
+          ),
+        );
+      },
+      
+    );
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFBEEFF9),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/calculator.png',
+              width: MediaQuery.of(context).size.width * 0.50,
+              height: MediaQuery.of(context).size.width * 0.50,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              'Body Health Calculator',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
